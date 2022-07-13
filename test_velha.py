1976f1003addfbd0479795e15893976edea66a0b
LdSaGL <lucas.sa.licio@gmail.com>
2022-07-12 22:32:40 -0300

    Testes para jogos impossíveis

--- test_velha.py
+++ test_velha.py
@@ -1,5 +1,19 @@
-#import pytest
 import velha
 
-def teste():
-    assert velha.JogoDaVelha([[2, 0, 1], [0, 2, 0], [1, 0, 1]]) == -1
+# Testes para jogos impossíveis 
+
+
+def testeJogoImpossivel_1():
+    assert velha.JogoDaVelha([[1, 1, 1], [1, 1, 1], [1, 1, 1]]) == -2
+
+
+def testeJogoImpossivel_2():
+    assert velha.JogoDaVelha([[2, 2, 2], [2, 2, 2], [2, 2, 2]]) == -2
+
+
+def testeJogoImpossivel_3():
+    assert velha.JogoDaVelha([[1, 1, 2], [2, 1, 1], [1, 0, 2]]) == -2
+
+
+def testeJogoImpossivel_4():
+    assert velha.JogoDaVelha([[2, 2, 0], [0, 2, 2], [1, 0, 1]]) == -2
