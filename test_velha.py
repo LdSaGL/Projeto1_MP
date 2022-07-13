e47b4c3e909a8665deaa703d6fea26d73a637667
LdSaGL <lucas.sa.licio@gmail.com>
2022-07-12 22:45:35 -0300

    Erro jogo com dois ganhadores

--- test_velha.py
+++ test_velha.py
@@ -19,6 +19,10 @@ def testeJogoImpossivel_4():
     assert velha.JogoDaVelha([[2, 2, 0], [0, 2, 2], [1, 0, 1]]) == -2
 
 
+def testeJogoImpossivel_5():
+    assert velha.JogoDaVelha([[2, 2, 2], [1, 1, 1], [0, 0, 0]]) == -2
+
+
 # Testes para jogos indefinidos
 
 
@@ -38,4 +42,4 @@ def testeJogoIndefinido_3():
 
 
 def testeJogoEmpatado():
-    assert velha.JogoDaVelha([[1, 2, 2], [2, 1, 1], [1, 1, 2]])
+    assert velha.JogoDaVelha([[1, 2, 2], [2, 1, 1], [1, 1, 2]]) == 0
