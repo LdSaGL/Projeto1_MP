808105fbc4dde0c263c198ccae82e68a20b97883
LdSaGL <lucas.sa.licio@gmail.com>
2022-07-12 22:39:11 -0300

    Testes para jogos indefinidos

--- test_velha.py
+++ test_velha.py
@@ -17,3 +17,18 @@ def testeJogoImpossivel_3():
 
 def testeJogoImpossivel_4():
     assert velha.JogoDaVelha([[2, 2, 0], [0, 2, 2], [1, 0, 1]]) == -2
+
+
+# Testes para jogos indefinidos
+
+
+def testeJogoIndefinido_1():
+    assert velha.JogoDaVelha([[0, 0, 0], [0, 0, 0], [0, 0, 0]]) == -1
+
+
+def testeJogoIndefinido_2():
+    assert velha.JogoDaVelha([[0, 0, 0], [1, 2, 1], [2, 1, 2]]) == -1
+
+
+def testeJogoIndefinido_3():
+    assert velha.JogoDaVelha([[1, 2, 0], [2, 1, 0], [2, 1, 0]]) == -1
