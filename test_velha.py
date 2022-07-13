f02368e7e159e23f05faea77deade3b1533ed8bc
LdSaGL <lucas.sa.licio@gmail.com>
2022-07-12 23:00:45 -0300

    Testes para X vencedor

--- test_velha.py
+++ test_velha.py
@@ -43,3 +43,20 @@ def testeJogoIndefinido_3():
 
 def testeJogoEmpatado():
     assert velha.JogoDaVelha([[1, 2, 2], [2, 1, 1], [1, 1, 2]]) == 0
+
+
+# Testes para jogos com X ganhador
+
+
+def testeJogoVencedorX_1():
+    assert velha.JogoDaVelha([[1, 0, 0], [0, 1, 2], [0, 2, 1]]) == 1
+
+
+def testeJogoVencedorX_2():
+    assert velha.JogoDaVelha([[0, 2, 0], [1, 1, 1], [2, 0, 2]]) == 1
+
+
+def testeJogoVencedorX_3():
+    assert velha.JogoDaVelha([[1, 2, 0], [1, 1, 2], [1, 2, 0]]) == 1
+
+
